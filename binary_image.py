692f630f063e0067cee9b9be52b904e580c7263c
Oddy <vinux.edu@gmail.com>
2020-05-30 13:09:44 +0700

    + binary_image

---
+++ binary_image.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('images/sample.jpg')
+image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+h,w = image.shape
+
+gr = np.zeros([h,w], dtype=np.uint8)
+
+t = 110
+for y in range(0, h):
+  for x in range(0, w):
+    v = int(img[y,x,0])
+    if v < t:
+      gr[y,x] = 0
+    elif v >= t:
+      gr[y, x] = 255
+
+cv2.imshow('gray',gr)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
